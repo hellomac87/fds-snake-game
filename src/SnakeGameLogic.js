02707dd6bedc770a665a45f388a90ea3295fe327
hellomac87 <hellomac87@gmail.com>
2018-10-05 16:43:26 +0900

    뱀만들기 1일차

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -10,28 +10,32 @@ function SnakeGameLogic() {
     {x: 1, y: 0},
     {x: 0, y: 0},
   ];
-
+  this.keys = 'right';
   // 먹이의 좌표
   this.fruit = {x: 3, y: 5};
 }
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
+  this.keys = "up";
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
+  this.keys = "down";
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
+  this.keys = "left";
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
+  this.keys = "right";
   console.log('right');
 }
 
@@ -39,8 +43,41 @@ SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
+  if(this.keys === 'down'){
+    this.joints.unshift({
+      x: this.joints[0].x,
+      y: this.joints[0].y + 1
+    });
+    this.joints.pop();
+  }
+  if (this.keys === 'up') {
+    this.joints.unshift({
+      x: this.joints[0].x,
+      y: this.joints[0].y - 1
+    });
+    this.joints.pop();
+  }
+  if (this.keys === 'right') {
+    this.joints.unshift({
+      x: this.joints[0].x + 1,
+      y: this.joints[0].y
+    });
+    this.joints.pop();
+  }
+  if (this.keys === 'left') {
+    this.joints.unshift({
+      x: this.joints[0].x - 1,
+      y: this.joints[0].y
+    });
+    this.joints.pop();
+  }
+
+  if (this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y){
+    this.joints
+  }
   console.log(`nextState`);
   return true;
 }
+
 
 export default SnakeGameLogic;
